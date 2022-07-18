2db19cc695efaf0d1677684319f422fecb944a07
leechaewon21 <codnjs991122@gmail.com>
2022-07-18 17:45:17 +0900

    Update plus two nums function

--- GitStudying/InputTwoNums.java
+++ GitStudying/InputTwoNums.java
@@ -1,6 +1,7 @@
 package modulization;
 
 import java.util.Scanner;
+import modulization.PlusFunction;
 
 public class InputTwoNums {
 
@@ -13,6 +14,9 @@ public class InputTwoNums {
 		num1 = scan.nextInt();
 		num2 = scan.nextInt();
 		
-		System.out.printf("Print Two Num : %d %d \n",num1,num2);
+		System.out.printf("Print Two Nums : %d %d \n",num1,num2);
+		
+		int sum = PlusFunction.plusTwoNums(num1,num2);
+		System.out.printf("Plus Two Nums : %d \n", sum);
 	}
 }
---
+++ GitStudying/PlusFunction.java
@@ -0,0 +1,7 @@
+package modulization;
+
+public class PlusFunction{
+	public static int plusTwoNums(int num1, int num2) {
+		return num1+num2;
+	}
+}

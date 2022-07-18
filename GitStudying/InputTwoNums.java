a050176c2209d0bc0276a18df0b263c327f8a4d7
leechaewon21 <codnjs991122@gmail.com>
2022-07-18 18:26:17 +0900

    Update minus two nums function

--- GitStudying/InputTwoNums.java
+++ GitStudying/InputTwoNums.java
@@ -2,6 +2,8 @@ package modulization;
 
 import java.util.Scanner;
 
+import modulization.MinusFunction;
+
 public class InputTwoNums {
 
 	static int num1, num2;
@@ -13,6 +15,9 @@ public class InputTwoNums {
 		num1 = scan.nextInt();
 		num2 = scan.nextInt();
 		
-		System.out.printf("Print Two Num : %d %d \n",num1,num2);
+		System.out.printf("Print Two Nums : %d %d \n",num1,num2);
+		
+		int diff = MinusFunction.minusTwoNums(num1,num2);
+		System.out.printf("Minus Two Nums : %d \n", diff);
 	}
 }
